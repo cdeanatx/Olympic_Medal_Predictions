f993d156761239e04a7290f19fceec308ee91cc9
Curtis Dean <curtis.dean@pilytix.com>
2022-01-12 12:51:02 -0600

    Refactor Elizabeth's Basic-EDA.py to show outputs using print() statements, clean up code formatting, and save all Seaborn plots to the plots/ folder

--- exploratory_data_analysis/Basic-EDA.py
+++ exploratory_data_analysis/Basic-EDA.py
@@ -3,107 +3,82 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-olympic_data = "data.csv"
-
-# Step 1: Review the dataset
+olympic_data = "resources/recent_olympic_data.csv"
 olympic_df = pd.read_csv(olympic_data)
-olympic_df.head()
 
-# Step 1: Review the dataset
-olympic_df.tail()
+# Explore general information about the dataset
+print(f'Shape: {olympic_df.shape}\n')
+print(f'Stats for numeric columns:\n\n{olympic_df.describe()}\n')
 
-# Step 1: Review the dataset
-olympic_df.shape
+# Explore data by columns
+print(f'Counts of unique values in each column:\n\n{olympic_df.nunique()}\n')
+print(f'List of values present in the "medal" column: {olympic_df["medal"].unique()}\n')
+print(f'List of values present in the "region" column:\n\n{(olympic_df["region"].sort_values().unique())}\n')
+print(f'List of values present in the "year" column:\n\n{olympic_df["year"].sort_values().unique()}\n')
 
-# Step 2: Understand the dataset, unique values, and missing values
-olympic_df.describe()
+# Count missing values by column
+print(f'Count of missing values by column:\n\n{olympic_df.isnull().sum()}\n')
 
-# Step 2: Understand the dataset, unique values, and missing values
-olympic_df.nunique()
+# Drop region abbreviations
+olympic2_df = olympic_df.drop('noc', axis=1)
 
-# Step 2: Understand the dataset, unique values, and missing values
-olympic_df['medal'].unique()
-
-# Step 2: Understand the dataset, unique values, and missing values
-olympic_df['region'].unique()
-
-# Step 2: Understand the dataset, unique values, and missing values
-olympic_df['year'].unique()
-
-# Step 2: Understand the dataset, unique values, and missing values
-olympic_df.isnull().sum()
-
-# Step 3: Cleaning the data
-olympic2_df = olympic_df.drop('noc', axis =1)
-olympic2_df.head()
-
-# Step 4: Relationship Analysis
-corelation = olympic2_df.corr()
+# How correlated are each of our variables?
+corelation = olympic2_df.corr() # Strong correlation (0.8) between height and weight. Everything else uncorrelated.
 sns.heatmap(corelation, xticklabels=corelation.columns, yticklabels=corelation.columns, annot=True)
+plt.savefig('exploratory_data_analysis/plots/correlation_heatmap.png')
 
-# Step 4: Relationship Analysis
-olympic2_df.dtypes
+sns.pairplot(olympic2_df.drop('id', axis=1))
+plt.savefig('exploratory_data_analysis/plots/correlation_pairplot.png')
 
-# Step 4: Relationship Analysis
-sns.pairplot(olympic2_df)
+# What are the data types in each column?
+print(f'Column datatypes:\n\n{olympic2_df.dtypes}\n')
 
-# Step 4: Relationship Analysis
+# How are height/weight related to sex, year, and season?
 sns.relplot(x= 'height', y='weight', hue='sex', data = olympic2_df)
+plt.savefig('exploratory_data_analysis/plots/height_weight_by_sex.png')
 
-# Step 4: Relationship Analysis
 sns.relplot(x= 'height', y='weight', hue='year', data = olympic2_df)
+plt.savefig('exploratory_data_analysis/plots/height_weight_by_year.png')
 
-# Step 4: Relationship Analysis
 sns.relplot(x= 'height', y='weight', hue='season', data = olympic2_df)
+plt.savefig('exploratory_data_analysis/plots/height_weight_by_season.png')
 
-# Step 5: Sports/Event Analysis
-sports_df = olympic2_df.groupby(['sport'])['event','id','year'].nunique()
-sports_df
+# Count unique events, participants, and years by sport
+sports_df = olympic2_df.groupby(['sport'])[['event','id','year']].nunique().rename(columns={'event': 'n_events', 'id': 'n_participants', 'year': 'n_years'})
+print(f'Unique counts by sport:\n\n{sports_df}\n')
 
-# Step 5: Sports/Event Analysis
-athletics_filter = (olympic2_df['sport'] == 'Athletics')
-athletics_df = olympic2_df[athletics_filter]
-athletics_df
+# Count unique participants and years by event, where sport is 'Athletics'
+athletics_df = olympic2_df[olympic2_df['sport'] == 'Athletics']
+athletics2_df = athletics_df.groupby(['event'])[['id','year']].nunique().rename(columns={'id': 'n_participants', 'year': 'n_years'})
+print(f'Unique counts by event, where sport is "Athletics":\n\n{athletics2_df}\n')
 
-# Step 5: Sports/Event Analysis
-athletics2_df = athletics_df.groupby(['event'])['id','year'].nunique()
-athletics2_df
-
-# Step 5: Athletics/Male Height-Weight by Event
-mens_athletics_filter = (athletics_df['sex'] == 'M')
-mens_athletics_df = athletics_df[mens_athletics_filter]
-
+# Filter athletics df for male/female and assess height/weight by event
+mens_athletics_df = athletics_df[athletics_df['sex'] == 'M']
 sns.relplot(x= 'height', y='weight', hue='event', data = mens_athletics_df)
+plt.savefig('exploratory_data_analysis/plots/male_athletics_height_weight_by_event.png')
 
-# Step 5: Athletics/Female Height-Weight by Event
-womens_athletics_filter = (athletics_df['sex'] == 'F')
-womens_athletics_df = athletics_df[womens_athletics_filter]
-
+womens_athletics_df = athletics_df[athletics_df['sex'] == 'F']
 sns.relplot(x= 'height', y='weight', hue='event', data = womens_athletics_df)
+plt.savefig('exploratory_data_analysis/plots/female_athletics_height_weight_by_event.png')
 
-# Step 5: Sports/Event Analysis
-athletics3_df = athletics_df.groupby(['event'])['age','weight','height'].mean()
-athletics3_df
+# Average age, weight, height by event, where sport is 'Athletics'
+athletics3_df = athletics_df.groupby(['event'])[['age','weight','height']].mean()
+print('Average age, weight, and height by event:\n\n{athletics3_df}\n')
 
-# Step 5: Sports/Event Analysis
-swimming_filter = (olympic2_df['sport'] == 'Swimming')
-swimming_df = olympic2_df[swimming_filter]
-swimming_df
+# Count unique participants and years by event, where sport is 'Swimming'
+swimming_df = olympic2_df[olympic2_df['sport'] == 'Swimming']
+swimming2_df = swimming_df.groupby(['event'])[['id','year']].nunique().rename(columns={'id': 'n_participants', 'year':'n_years'})
+print(f'Unique counts by event, where sport is "Swimming":\n\n{swimming2_df}\n')
 
-# Step 5: Sports/Event Analysis
-swimming2_df = swimming_df.groupby(['event'])['id','year'].nunique()
-swimming2_df
-swimming3_df = swimming_df.groupby(['event'])['age','weight','height'].mean()
-swimming3_df
+# Average age, weight, height by event, where sport is 'Swimming'
+swimming3_df = swimming_df.groupby(['event'])[['age','weight','height']].mean()
+print(f'Avg age, weight, height by event, where sport is "Swimming":\n\n{swimming3_df}\n')
 
-# Step 5: Swimming/Male Height-Weight by Event
-mens_swimming_filter = (swimming_df['sex'] == 'M')
-mens_swimming_df = swimming_df[mens_swimming_filter]
-
+# Filter swimming df for male/female and assess height/weight by event
+mens_swimming_df = swimming_df[swimming_df['sex'] == 'M']
 sns.relplot(x= 'height', y='weight', hue='event', data = mens_swimming_df)
+plt.savefig('exploratory_data_analysis/plots/male_swimming_height_weight_by_event.png')
 
-# Step 5: Swimming/Female Height-Weight by Event
-womens_swimming_filter = (swimming_df['sex'] == 'F')
-womens_swimming_df = swimming_df[womens_swimming_filter]
-
+womens_swimming_df = swimming_df[swimming_df['sex'] == 'F']
 sns.relplot(x= 'height', y='weight', hue='event', data = womens_swimming_df)
+plt.savefig('exploratory_data_analysis/plots/female_swimming_height_weight_by_event.png')
