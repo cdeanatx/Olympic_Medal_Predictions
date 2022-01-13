c9231f37b173123fa1a123c01715059830212909
elisabethgnewman <88783255+elisabethgnewman@users.noreply.github.com>
2022-01-12 20:29:04 -0600

    Update Biometrics_Event.py

--- exploratory_data_analysis/Biometrics_Event.py
+++ exploratory_data_analysis/Biometrics_Event.py
@@ -17,7 +17,32 @@ olympic2_df.head()
 
 olympic2_df[olympic2_df.sport=="Athletics"].groupby(["sex", "event"])["height", "weight", "age"].mean()
 
+athletics_filter = (olympic2_df['sport'] == 'Athletics')
+athletics_df = olympic2_df[athletics_filter]
+athletics_df
+mens_athletics_filter = (athletics_df['sex'] == 'M')
+mens_athletics_df = athletics_df[mens_athletics_filter]
+fig, ax = plt.subplots(figsize=(10, 10))
+sns.histplot(data=mens_athletics_df, x='height', hue='event', stat='count', edgecolor=None)
+ax.set_title('Mens Athletics Height Dist')
+
+fig, ax = plt.subplots(figsize=(10, 10))
+sns.histplot(data=mens_athletics_df, x='weight', hue='event', stat='count', edgecolor=None)
+ax.set_title('Mens Athletics Weight Dist')
+
 olympic2_df[olympic2_df.sport=="Swimming"].groupby(["sex", "event"])["height", "weight", "age"].mean()
+
+swimming_filter = (olympic2_df['sport'] == 'Swimming')
+swimming_df = olympic2_df[swimming_filter]
+swimming_df
+mens_swimming_filter = (swimming_df['sex'] == 'M')
+mens_swimming_df = swimming_df[mens_swimming_filter]
+fig, ax = plt.subplots(figsize=(10, 10))
+sns.histplot(data=mens_swimming_df, x='height', hue='event', stat='count', edgecolor=None)
+ax.set_title('Mens Swimming Height Dist')
+
+sns.histplot(data=mens_swimming_df, x='weight', hue='event', stat='count', edgecolor=None)
+ax.set_title('Mens Swimming Weight Dist')
 
 olympic2_df[olympic2_df.sport=="Shooting"].groupby(["sex", "event"])["height", "weight", "age"].mean()
 
