8fa9ed45957c93e8af86f67930488dd2e32e5897
elisabethgnewman <88783255+elisabethgnewman@users.noreply.github.com>
2022-01-12 20:10:07 -0600

    Skeleton of biometrics by event in major sports

---
+++ exploratory_data_analysis/Biometrics_Event.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+olympic_data = "data.csv"
+
+olympic_df = pd.read_csv(olympic_data)
+olympic_df.head()
+
+sports_df = olympic2_df.groupby(['sport'])['event','id','year'].nunique().sort_values(by='event', ascending=False)
+sports_df
+
+olympic2_df = olympic_df.drop('noc', axis =1)
+olympic2_df.head()
+
+olympic2_df[olympic2_df.sport=="Athletics"].groupby(["sex", "event"])["height", "weight", "age"].mean()
+
+olympic2_df[olympic2_df.sport=="Swimming"].groupby(["sex", "event"])["height", "weight", "age"].mean()
+
+olympic2_df[olympic2_df.sport=="Shooting"].groupby(["sex", "event"])["height", "weight", "age"].mean()
+
+olympic2_df[olympic2_df.sport=="Cycling"].groupby(["sex", "event"])["height", "weight", "age"].mean()
