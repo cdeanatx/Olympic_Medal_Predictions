3e4ab41b3d199dc1c0faca7251a983eabe3750c7
Nikhila999 <40877603+Nikhila999@users.noreply.github.com>
2022-01-13 23:31:00 -0600

    changed the encoder, added predict method and imputed values

--- Olympic_Analysis_PrecictionsV3.py
+++ Olympic_Analysis_PrecictionsV3.py
@@ -27,27 +27,24 @@ column_null_counts
 olympic_data["medal"].value_counts()
 # 172495 null values
 
-# Replace NAs in medal column with 0
 olympic_data["medal"] = olympic_data["medal"].fillna(0)
 
-# Replace medal with 1
 olympic_data["is_medal"] = np.where(olympic_data["medal"] == 0, 0, 1)
 olympic_data
 
-# Check for null values in all columns
 olympic_data.isnull().sum(axis=0)
 
-# Remove event column from the dataframe
 olympic_data = olympic_data[["id","sex","year","season","sport","is_medal","age","height","weight"]]
 
-# Check datatypes for each column
 olympic_data.dtypes
 
 # Split the data
-X = olympic_data.drop(["is_medal"], axis=1).loc[0:999]
-y = np.array(olympic_data["is_medal"])[:1000]
+X = olympic_data.drop(["is_medal"], axis=1)
+y = np.array(olympic_data["is_medal"])
 
-# imports for encoding, imputing and modeleing 
+X = X.drop(["year"], axis=1)
+
+
 from numpy import isnan
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
@@ -57,13 +54,40 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedStratifiedKFold
 from numpy import mean
 from numpy import std
+
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OneHotEncoder
+from sklearn import preprocessing
+import category_encoders as ce
+
+
+# ### Using One Hot Encoder
+
+# Taking very long time to run
+
+# # define modeling pipeline
+# model = RandomForestClassifier()
+# imputer = IterativeImputer()
+# pipeline = Pipeline(steps=[("onehot", OneHotEncoder(sparse=False,handle_unknown='ignore')),
+#                            ('i', imputer), 
+#                            ('m', model)
+#                           ])
+
+# # define model evaluation
+# cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# # evaluate model
+# scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+
+# print('Mean Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
+
+
+# ### Using Target Encoder
 
 # define modeling pipeline
 model = RandomForestClassifier()
 imputer = IterativeImputer()
-pipeline = Pipeline(steps=[("onehot", OneHotEncoder(sparse=False,handle_unknown='ignore')),
+pipeline = Pipeline(steps=[("targetEncoder", ce.TargetEncoder(cols=['sex', 'season', 'sport'])),
                            ('i', imputer), 
                            ('m', model)
                           ])
@@ -76,17 +100,55 @@ scores = cross_val_score(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=-1, e
 
 print('Mean Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
 
-# Things to work on
 
-# 1. Use make_pipeline method 
+# ## Add Predict method and validate the data
 
-# 2. Check imputed values from IterativeImputer 
+# fit the pipeline with the training data
+pipeline.fit(X,y)
 
-# 3. Add Predict method and validate the data
+# predict target values on the training data
+x_predict = pipeline.predict(X)
 
-# 4. Add Ordinal regression to update the type of medal
+df = pd.DataFrame(x_predict, columns = ['medal_predictions'])
+df.value_counts()
 
-# 5. Add event category and remove sport
+# Actual medal column
+olympic_data["is_medal"].value_counts()
 
-# 6. Make sure the model runs for all records
+
+# ## Check imputed values from IterativeImputer 
+
+x_encoder = ce.TargetEncoder(cols=['sex', 'season', 'sport'])
+
+x_encoded = x_encoder.fit_transform(X,y)
+x_encoded
+
+x_encoded.isnull().sum(axis=0)
+
+imputer = IterativeImputer()
+
+imputer.fit(x_encoded)
+
+Xtrans = imputer.transform(x_encoded)
+
+impute_df = pd.DataFrame(Xtrans, columns = ['id',"sex","season","sport","age","height","weight"])
+impute_df
+
+impute_df.isnull().sum(axis=0)
+
+combined_df = pd.concat([x_encoded, impute_df[["age","height","weight"]]], axis=1)
+combined_df[combined_df.isna().any(axis=1)]
+
+
+# ## Use make_pipeline method 
+
+# ## Add recall
+
+# ## Add Ordinal regression to update the type of medal
+
+# ## Add event category and remove sport
+
+# ## Make sure the model runs for all records
+
+
 
